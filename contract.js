c7d911bc979218f0b08e97ec99841daabd15688c
Chris <clally1@bsbson.edu>
2021-11-08 21:54:07 -0500

    a

--- contract.js
+++ contract.js
@@ -7,23 +7,7 @@ import { web3 } from './connectWallet.js';
 export let address;
 export let abi;
 export let mintPrice;
-
-if (window?.WEBSITE_URL?.includes(window.location.hostname)) {
-  address = window.CONTRACT_ADDRESS;
-  abi = typeof window.CONTRACT_ABI === 'string'
-      ? JSON.parse(window.CONTRACT_ABI) : window.CONTRACT_ABI;
-} else if (window.location.hostname.includes('cryptowords')) {
-  address = wordsAddress;
-  abi = wordsAbi;
-} else if (window.location.hostname.includes('tigersfightclub.art')) {
-  address = tigerAddress;
-  abi = tigerAbi;
-} else if (window.location.hostname.includes('textapes.art')) {
   address = apesAddress;
   abi = apesAbi;
-} else if (window.location.hostname.includes('georgy-nft.webflow.io') || window.location.hostname.includes('app.nftmoon.space')) {
-  address = moonAddress;
-  abi = moonAbi;
-}
 
 export let contract = new web3.eth.Contract(abi, address);
